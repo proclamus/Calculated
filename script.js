42a45ffa642562ddcb77c9bb909780e2ce38c951
Proclamus <patrick.yasmim@gmail.com>
2022-07-10 13:48:43 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+const calculator = document.querySelector('.calculadora')
+const keys = calculator.querySelector('.teclas')
+
+keys.addEventListener('click', e => {
+    if(e.target.matches('button')) {
+        const key = e.target
+        const action = key.dataset.action
+        if (!action) {
+            console.log('number key!')
+          }
+          if (
+            action === 'somar' ||
+            action === 'subtrair' ||
+            action === 'multiplicar' ||
+            action === 'dividir'
+          ) {
+            console.log('operator key!')
+          }
+          if (action === 'decimal') {
+            console.log('decimal key!')
+          }
+          
+          if (action === 'clear') {
+            console.log('clear key!')
+          }
+          
+          if (action === 'calculate') {
+            console.log('equal key!')
+          }
+    }
+})
